d8243ab13de5ecf97b2c6caddf7ad566960c4c15
XiongQiuQiu <zhangjinwei94@163.com>
2016-11-28 16:28:44 +0800

    al

--- douban_book.py
+++ douban_book.py
@@ -103,16 +103,6 @@ def save_excel(book_lists, book_tag_lists):
 
 
 if __name__ == '__main__':
-    # book_tag_lists = ['心理','判断与决策','算法','数据结构','经济','历史']
-    # book_tag_lists = ['传记','哲学','编程','创业','理财','社会学','佛教']
-    # book_tag_lists = ['思想','科技','科学','web','股票','爱情','两性']
     book_tag_lists = ['计算机','机器学习','linux','android','数据库','互联网']
-    book_tag_lists = ['数学']
-    # book_tag_lists = ['摄影','设计','音乐','旅行','教育','成长','情感','育儿','健康','养生']
-    # book_tag_lists = ['商业','理财','管理']
-    #book_tag_lists = ['港台']
-    # book_tag_lists = ['科普','经典','生活','心灵','文学']
-    # book_tag_lists = ['科幻','思维','金融']
-    #book_tag_lists = ['个人管理', '时间管理', '投资', '文化', '宗教']
     book_lists = spider_start(book_tag_lists)
     save_excel(book_lists, book_tag_lists)
